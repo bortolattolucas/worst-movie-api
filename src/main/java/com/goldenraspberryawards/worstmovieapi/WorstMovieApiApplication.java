6b8cdf5ace53d135f4c76d8604af4e1acc1604b4
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2022-08-02 17:08:58 -0300

    Project created

---
+++ src/main/java/com/goldenraspberryawards/worstmovieapi/WorstMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.goldenraspberryawards.worstmovieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorstMovieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorstMovieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goldenraspberryawards/worstmovieapi/WorstMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenraspberryawards.worstmovieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorstMovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
